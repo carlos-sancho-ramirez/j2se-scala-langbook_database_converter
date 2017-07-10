8dd05ac8472778983e85543c8b1d7d335e3c4379
Carlos Sancho Ramírez <carlos.sancho.ramirez@gmail.com>
2017-07-10 18:20:53 +0200

    Compose basic collections for database v4 and dump it in WordRepresentations csv file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,8 @@
 import java.io._
 import java.sql.DriverManager
 
+import scala.collection.mutable.ArrayBuffer
+
 object Main {
 
   def filePath = {
@@ -9,7 +11,114 @@ object Main {
     new File(resource.toURI).getPath
   }
 
+  var conceptCount = 0
+
+  val enAlphabet = conceptCount
+  val esAlphabet = conceptCount + 1
+  val kanjiAlphabet = conceptCount + 2
+  val kanaAlphabet = conceptCount + 3
+
+  val alphabets: Vector[Int] = {
+    val i = conceptCount
+    val r = Vector(
+      i, // English
+      i + 1, // Spanish
+      i + 2, // Japanese kanji
+      i + 3 // Japanese kana
+    )
+
+    conceptCount += r.size
+    r
+  }
+
+  case class Language(concept: Int, code: String)
+
+  val languages: Vector[Language] = {
+    val i = conceptCount
+    val r = Vector(
+      Language(i, "en"), // English
+      Language(i + 1, "es"), // Spanish
+      Language(i + 2, "ja") // Japanese
+    )
+
+    conceptCount += r.size
+    r
+  }
+
+  val symbolArrays = ArrayBuffer[String]()
+
+  /**
+    * Checks if the given symbol array already exist in the list.
+    * If so, the index is returned. If not it is appended into
+    * the list and the index is returned.
+    */
+  def addSymbolArray(symbolArray: String): Int = {
+    if (symbolArray == null) {
+      throw new IllegalArgumentException()
+    }
+
+    // This should be optimized indexing the string instead of check if it exists one by one.
+    var found = -1
+    var i = 0
+    val size = symbolArrays.size
+    while (found < 0 && i < size) {
+      if (symbolArrays(i) == symbolArray) found = i
+      i += 1
+    }
+
+    if (found >= 0) found
+    else {
+      symbolArrays += symbolArray
+      size
+    }
+  }
+
+  case class WordRepresentation(word: Int, alphabet: Int, symbolArray: Int)
+
+  val representations = ArrayBuffer[WordRepresentation]()
+
+  var wordCount = 0
+
+  def appendEnglishWord(symbolArray: String): Unit = {
+    representations.append(WordRepresentation(wordCount, enAlphabet, addSymbolArray(symbolArray)))
+    wordCount += 1
+  }
+
+  def appendSpanishWord(symbolArray: String): Unit = {
+    representations.append(WordRepresentation(wordCount, esAlphabet, addSymbolArray(symbolArray)))
+    wordCount += 1
+  }
+
+  def appendJapaneseWord(kanjiSymbolArray: String, kanaSymbolArray:String): Unit = {
+    representations.append(WordRepresentation(wordCount, kanjiAlphabet, addSymbolArray(kanjiSymbolArray)))
+    representations.append(WordRepresentation(wordCount, kanaAlphabet, addSymbolArray(kanaSymbolArray)))
+    wordCount += 1
+  }
+
+  def initialiseDatabase(): Unit = {
+    appendEnglishWord("Language")
+    appendSpanishWord("Idioma")
+    appendJapaneseWord("言語", "げんご")
+
+    appendEnglishWord("English")
+    appendSpanishWord("Inglés")
+    appendJapaneseWord("英語", "えいご")
+
+    appendEnglishWord("Spanish")
+    appendSpanishWord("Español")
+    appendJapaneseWord("スペイン語", "スペイン")
+
+    appendEnglishWord("Japanese")
+    appendSpanishWord("Japonés")
+    appendJapaneseWord("日本語", "にほんご")
+
+    appendJapaneseWord("漢字", "かんじ")
+    appendJapaneseWord("平仮名", "かな")
+  }
+
   def main(args: Array[String]): Unit = {
+    initialiseDatabase()
+
     val outStream = new PrintWriter(new FileOutputStream("WordRegister.csv"))
     try {
       val path = filePath
@@ -19,14 +128,21 @@ object Main {
         val statement = connection.createStatement()
         statement.setQueryTimeout(10) // 10 seconds
         val resultSet = statement.executeQuery("SELECT * FROM WordRegister")
-        var limit = 100000
+        var limit = 10000
         while (limit > 0 && resultSet.next()) {
+          val kanjiSymbolArray = resultSet.getString("mWrittenWord")
+          val kanaSymbolArray = resultSet.getString("mPronunciation")
+          val spSymbolArray = resultSet.getString("meaning")
           val rowValues = List(
             resultSet.getInt("id"),
-            resultSet.getString("mWrittenWord"),
-            resultSet.getString("mPronunciation"),
-            resultSet.getString("meaning")
+            kanjiSymbolArray,
+            kanaSymbolArray,
+            spSymbolArray
           )
+
+          appendJapaneseWord(kanjiSymbolArray, kanaSymbolArray)
+          appendSpanishWord(spSymbolArray)
+
           outStream.println(rowValues.mkString(","))
           limit -= 1
         }
@@ -37,6 +153,18 @@ object Main {
     }
     finally {
       outStream.close()
+    }
+
+    val outStream2 = new PrintWriter(new FileOutputStream("WordRepr.csv"))
+    try {
+      var i = 0
+      for (repr <- representations) {
+        outStream2.println(s"$i,${repr.word},${repr.alphabet},${symbolArrays(repr.symbolArray)}")
+        i += 1
+      }
+    }
+    finally {
+      outStream2.close()
     }
   }
 }
