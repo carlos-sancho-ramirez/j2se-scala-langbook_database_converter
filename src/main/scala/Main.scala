cc125d26d14dfda415f99b29895f9df0244c0d79
Carlos Sancho Ramírez <carlos.sancho.ramirez@gmail.com>
2017-07-10 13:38:33 +0200

    Include basic file read as proof of concept

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,20 @@
+import java.io._
+
 object Main {
+
+  def filePath = {
+    val resource = getClass.getClassLoader.getResource("langbook.db")
+    if (resource == null) sys.error("Expected file src/main/resources/langbook.db not present")
+    new File(resource.toURI).getPath
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
+    val stream = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))
+    try {
+      println(stream.readLine())
+    }
+    finally {
+      stream.close()
+    }
   }
 }
