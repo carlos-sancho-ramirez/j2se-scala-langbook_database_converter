e4f91d0f89616771df18be6d8fe149c5518f57b8
Carlos Sancho Ramírez <carlos.sancho.ramirez@gmail.com>
2017-07-10 14:32:54 +0200

    Dump WordRegister table in a csv file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,21 +10,33 @@ object Main {
   }
 
   def main(args: Array[String]): Unit = {
-    val path = filePath
-    println("Connecting to database at " + path)
-    val connection = DriverManager.getConnection("jdbc:sqlite:" + path)
+    val outStream = new PrintWriter(new FileOutputStream("WordRegister.csv"))
     try {
-      val statement = connection.createStatement()
-      statement.setQueryTimeout(10) // 10 seconds
-      val resultSet = statement.executeQuery("SELECT * FROM WordRegister")
-      var limit = 20
-      while (limit > 0 && resultSet.next()) {
-        println(resultSet.getString("mWrittenWord"))
-        limit -= 1
+      val path = filePath
+      println("Connecting to database at " + path)
+      val connection = DriverManager.getConnection("jdbc:sqlite:" + path)
+      try {
+        val statement = connection.createStatement()
+        statement.setQueryTimeout(10) // 10 seconds
+        val resultSet = statement.executeQuery("SELECT * FROM WordRegister")
+        var limit = 100000
+        while (limit > 0 && resultSet.next()) {
+          val rowValues = List(
+            resultSet.getInt("id"),
+            resultSet.getString("mWrittenWord"),
+            resultSet.getString("mPronunciation"),
+            resultSet.getString("meaning")
+          )
+          outStream.println(rowValues.mkString(","))
+          limit -= 1
+        }
+      }
+      finally {
+        connection.close()
       }
     }
     finally {
-      connection.close()
+      outStream.close()
     }
   }
 }
